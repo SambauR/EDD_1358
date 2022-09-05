b9085975e95d4e7609652f3950c255c1403babb0
Samanta Bautista <sambau16@gmail.com>
2022-09-05 04:19:31 -0500

    esta es una prueba

---
+++ prueba/src/main/java/NewMain.java
@@ -0,0 +1,10 @@
+
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola prueba");    }
+    
+}
